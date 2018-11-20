083cabd9056efab3764a6d1067ce9992fc86904b
Bob Ingram <bob@theparamountgroup.us>
2018-11-20 08:03:16 -0500

    saving changes for safety

--- BlockController.js
+++ BlockController.js
@@ -1,11 +1,11 @@
-const SHA256 = require('crypto-js/sha256');
+
 const BlockClass = require('./Block.js');
 const BlockChain = require('./BlockChain.js')
 
 
-/**
+/********************************************************************
  * Controller Definition to encapsulate routes to work with blocks
- */
+ ********************************************************************/
 
 class BlockController {
 
@@ -15,122 +15,70 @@ class BlockController {
      */
 
     constructor(app) {
-        console.log("in BlockController constructor");
         this.app = app;
-        //this.blocks = [];
-        //  this.initializeMockData();
         this.blockChain = new BlockChain.Blockchain();
         this.getBlockByIndex();
         this.postNewBlock();
-        console.log("finished BlockController constructor");
-
     }
 
-    /**
+    /*************************************************************************************
      * Implement a GET Endpoint to retrieve a block by index, url: "/api/block/:index"
-     */
+     *************************************************************************************/
 
     getBlockByIndex() {
-
-
-        console.log("in getBlockByIndex this.currentBlockHeight is: " + this.blockChain.getBlockHeight());
         this.app.get("/api/block/:height?", (req, res) => {
+            // Listen for height param and convert to integer if necessary
             let height = parseInt(req.params.height, 10);
-            console.log("in getBlockByIndex height requested is: " + height);
+            // start error checking if ok send back requested block in json format
             this.blockChain.getBlockHeight().then((blockHeight) => {
-
-
-                console.log("in getBlockByIndex and blockHeight is: " + blockHeight);
-                console.log("in getBlockByIndex and height is: " + height);
+                // check if height is not a number
                 let notNumber = isNaN(height);
-                console.log("boolean test : " + notNumber);
                 if ((height > blockHeight) || (height < 0) || notNumber) {
-                    console.log(" in getBlockByIndex and blockHeight is: " + blockHeight + " height is: " + height);
+                    //requested height is outside the blockchain height or NaN send 404 error
                     res.status(404).send("Block height parameter is out of bounds or NaN");
                 } else {
-
+                    // return requested block
                     this.blockChain.getBlock(height).then((block) => {
-                        if (block) { //  verify here if the block exist if not return 404
+                        if (block) { //  requested block retrieved send back in json with 200 status
                             return res.status(200).json(block);
                         } else {
-                            return res.status(404).send("Block Not Found!");
+                            return res.status(500).send("Block Not Found Unknown Reason");
                         }
                     })
-
-                }
-            })
+                } //catch error if something went wrong with promises
+            }).catch((error) => { return res.status(500).send("Something went wrong! " + error); })
         })
-
     }
 
-    /*
-    
-    console.log("in getBlockByIndex this.currentBlockHeight is: " + this.blockChain.getBlockHeight());
-    this.app.get("/api/block/:height?", (req, res) => {
-        let height = parseInt(req.params.height, 10);
-        console.log("in getBlockByIndex height requested is: " + height);
-        let blockHeight = this.blockChain.getBlockHeight();
-        
-        console.log("in getBlockByIndex and currentBlockHeight is: " + blockHeight);
-        if (height > blockHeight || height < blockHeight || NaN) {
-            res.status(404).send("Block height parameter is out of bounds or NaN");
-           
-        };
-        if (req.params.height) {//Verify here if the parameter is correct
-            const height = req.params.height;
-            this.blockChain.getBlock(height).then((block) => {
-                if (block) { //  verify here if the block exist if not return 404
-                    return res.status(200).json(block);
-                } else {
-                    return res.status(404).send("Block Not Found!");
-                }
-            }).catch((error) => { return res.status(500).send("Something went wrong!   " + error); })
-        } else {
-            return res.status(404).send("Block Not Found!");
-        }
- 
-    });
-    */
-
-
-
-
-    /**
+    /**********************************************************************
     * Implement a POST Endpoint to add a new Block, url: "/api/block"
-    */
+    ***********************************************************************/
 
     postNewBlock() {
         this.app.post("/api/block/:data?", (req, res) => {
-            console.log("what is req.params.data: " + req.params.data);
+            // retrieve data and create new block
             if (req.params.data) {
                 let newBlock = new BlockClass.Block(req.params.data);
+                // add block and check for errors
                 this.blockChain.addBlock(newBlock).then((addedBlock) => {
-                    if (addedBlock) {
-                        return res.status(200).json(addedBlock);
+                    if (addedBlock) {// success return block that was added
+                        return res.status(201).json(addedBlock);
                     } else {
                         return res.status(500).send("Something went wrong block was NOT added");
                     }
-                }).catch((error) => {
+                }).catch((error) => {//catch error if something went wrong with promises
                     return res.status(500).send("Something went wrong! " + error);
-                    //console.log(error);
                 })
-            } else {
+            } else {// data param was empty
                 res.status(403).send("Forbidden to persist block without content");
-
             }
         });
     }
 }
 
-
-
-
-
-
-//let myBlockChain = new BlockChain.Blockchain();
-
-/**
+/**************************************************************
  * Exporting the BlockController class
  * @param {*} app 
- */
+ **************************************************************/
+
 module.exports = (app) => { return new BlockController(app); }
